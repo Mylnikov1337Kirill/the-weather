add4ddcd96cfc3942aa5a50ca131d01c2647586c
mylnikov1337kirill <mylnikov1337kirill@gmail.com>
2020-06-27 12:45:43 +0300

    make build available for deploying at github pages

---
+++ docs/precache-manifest.759f85054385281d9e565465d0c8d9f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba59d4541068ef2412f412c32644e5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "842484293a8ac82b41df",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c119c66337a4279a93a",
+    "url": "/static/js/2.a2dd8e0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2dd8e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842484293a8ac82b41df",
+    "url": "/static/js/main.8cfdf5ba.chunk.js"
+  },
+  {
+    "revision": "62f56d3dd1f61d1eb184",
+    "url": "/static/js/runtime-main.6a5cb73f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
