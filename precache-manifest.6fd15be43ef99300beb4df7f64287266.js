f718e13a8f83d3e51c0bf49fe6df911ab2272ff3
mylnikov1337kirill <mylnikov1337kirill@gmail.com>
2020-06-27 12:53:00 +0300

    Updates

---
+++ precache-manifest.6fd15be43ef99300beb4df7f64287266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3defafa967b7ed15a0202aa6d1311f78",
+    "url": "/the-weather/index.html"
+  },
+  {
+    "revision": "8dd4238b66606ae20606",
+    "url": "/the-weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c119c66337a4279a93a",
+    "url": "/the-weather/static/js/2.a2dd8e0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-weather/static/js/2.a2dd8e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd4238b66606ae20606",
+    "url": "/the-weather/static/js/main.3f06767b.chunk.js"
+  },
+  {
+    "revision": "d59294e9eb8006a0caa9",
+    "url": "/the-weather/static/js/runtime-main.5f41718b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
