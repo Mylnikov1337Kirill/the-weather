13838815521fbebf71d93feb63fe651851d41f11
mylnikov1337kirill <mylnikov1337kirill@gmail.com>
2020-07-01 00:13:35 +0300

    Updates

---
+++ precache-manifest.073fbdfe18dc3dc74ec24afc099a441a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a277076c4c29a320017156f20323247a",
+    "url": "/the-weather/index.html"
+  },
+  {
+    "revision": "5cf5ed5967f0d1bc7301",
+    "url": "/the-weather/static/css/main.4707799f.chunk.css"
+  },
+  {
+    "revision": "d21a94b793150a97dcf1",
+    "url": "/the-weather/static/js/2.c71c00a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-weather/static/js/2.c71c00a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf5ed5967f0d1bc7301",
+    "url": "/the-weather/static/js/main.9130a0dc.chunk.js"
+  },
+  {
+    "revision": "d59294e9eb8006a0caa9",
+    "url": "/the-weather/static/js/runtime-main.5f41718b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/the-weather/precache-manifest.8d048035892957d09b3e0442ce0fba4a.js"
+  "/the-weather/precache-manifest.073fbdfe18dc3dc74ec24afc099a441a.js"
 );
 
 self.addEventListener('message', (event) => {
